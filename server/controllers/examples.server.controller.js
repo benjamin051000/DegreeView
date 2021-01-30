3c96cb2be386e510dd7a6c6d59255174c953bec6
Joshua Haddad <joshuahaddad@ufl.edu>
2021-01-30 15:29:51 -0500

    course processing

--- server/controllers/examples.server.controller.js
+++ server/controllers/examples.server.controller.js
@@ -3,7 +3,7 @@ const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 function get_course(course_code){
   var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open("GET", "https://one.ufl.edu/apix/soc/schedule/?category=CWSP&term=2211&last-row=1&course_code&course-code=" + course_code, false ); // false for synchronous request
+  xmlHttp.open("GET", "https://one.ufl.edu/apix/soc/schedule/?category=CWSP&term=2211&course_code&course-code=" + course_code, false ); // false for synchronous request
   xmlHttp.send( null );
 //   console.log(xmlHttp)
   return xmlHttp.responseText;
@@ -19,20 +19,37 @@ function get_course(course_code){
 //     let url = `http://one.ufl.edu/apix/soc/schedule/?category=${category}&term=${term}`;
 
 //     url += `&course-code=${courseCode}`;
-    
+
 //     console.log("Url:", url);
-    
+
 //     const response = await fetch(url);
 //     console.log("Response received.");
 
 //     const data = await response.json();
 //     console.log("JSON parsed.");
-    
+
 //     return data; // TODO return data[0] ?
 // }
 
+function process_course(course_code){
+  var course_info = get_course(course_code);
+  var course = JSON.parse(course_info.substring(1, course_info.length-1))["COURSES"][0];
 
+  var info = course['prerequisites'].split(";");
+
+  var course = {
+    name: course['name'],
+    code: course['code'],
+    desc: course['description'],
+    credits: parseInt(info[0].match(/\d\d?/g)[0]),
+    prereq: info[1],
+    coreq: info[2]
+  };
+  
+  return course
+}
 exports.getCourseInfo = async function(req, res) {
     // res.send(await fetchCourses(req.courseCode));
     res.send(get_course(req.params.courseCode));
 }
+process_course("PHY2048");
