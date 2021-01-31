cd1029d9d87666395c7f1826bfc128af076e9f9e
Austin Welch <66348244+AustinWelch@users.noreply.github.com>
2021-01-30 20:54:26 -0500

    Header includes modal buttons

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,22 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 import twouf from '../../assets/TWOUF.jpg'
-
+import NewModal from '../NewModal.js';
+import ExportModal from '../ExportModal.js';
 
 //Set website header, includes the bar with home 
 const Header = () => {
+    const exportFunction = () => {
+        alert("exporting!!")
+    }
 
-
+    
     return (
         <div style={{height:"60px", backgroundColor:"rgb(40, 87, 151)", borderBottom:"3px solid rgb(224, 129, 46)"}} className="ui fixed menu">
            
                 <div className="ui fluid container">
-                    <a href="#" className="header item" style={{font:"font-size: 1.5rem font-family: Roboto font-weight: 400 line-height: 1.334", color:"white"}}>
+                    <a style={{color:"white"}} href="#" className="header item" >
                     <img style={{marginRight:"20px"}} className="logo" src={twouf} alt="TWODOTUF"/>
                         TwoDotUF
                     </a>
+                    <ExportModal className="ui fixed menu"/>
+                    <NewModal className="ui fixed menu"/>
                 </div>
-            
+                
+               
+                
+      
+
         </div>
     )
 }
