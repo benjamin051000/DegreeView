102026404c41436a8a4c8c7fb6a8aa44566796da
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2021-01-31 03:28:31 -0500

    updated to match new Home.js

--- client/src/views/Home/OverviewFlow.js
+++ client/src/views/Home/OverviewFlow.js
@@ -3,38 +3,36 @@ import ReactFlow, {
   removeElements,
   addEdge,
 } from 'react-flow-renderer';
-import initialElements from './initial-elements';
 
 const onLoad = (reactFlowInstance) => {
   console.log('flow loaded:', reactFlowInstance);
   reactFlowInstance.fitView();
 };
 
-const OverviewFlow = () => {
-  const [elements, setElements] = useState(initialElements);
+const OverviewFlow = ({nodes, setNodes}) => {
 
   const onElementsRemove = (elementsToRemove) =>
-  setElements((els) => removeElements(elementsToRemove, els));
+  setNodes((els) => removeElements(elementsToRemove, els));
 
-  const onConnect = (params) => setElements((els) => addEdge(params, els));
+  const onConnect = (params) => setNodes((els) => addEdge(params, els));
   const onElementClick = (event, element) => console.log('click', element);
   const onNodeDragStop = (event, node) => console.log('drag stop', node);
 
   return (
     <ReactFlow
-      elements={elements}
+      elements={nodes}
       nodesConnectable={false}
       onElementsRemove={onElementsRemove}
       onConnect={onConnect}
       onLoad={onLoad}
       snapToGrid={true}
+      snapGrid={[225, 133]}
       paneMoveable={false}
       snapGrid={[1,1]}
       onNodeDragStop={onNodeDragStop}
       onElementClick={onElementClick}
       deleteKeyCode={46}
-    >
-    </ReactFlow>
+    />
   );
 };
 
