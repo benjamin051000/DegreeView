a6d67564f5bf283d9e7a0a7adb99113dee82211b
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2021-01-31 20:18:30 -0500

    documented

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,11 +1,19 @@
-// import XMLHttpRequest from "xmlhttprequest";
 import db from '../config/DBConfig.js';
-// import https from 'https';
+
 import __getCourseInfo from '../functions/GetCourseInfo.js';
 
 import { runFlow, process_course } from '../functions/GetSemester.js';
 
-const retrieveRequirements = async function (req, res) {
+
+/**
+ * Accesses database of major requirement courses, and 
+ * returns an array of required courses for a given major.
+ * @param {object} req - request includes a major 
+ * (which currently must follow database naming convention)
+ * @param {object} res - response sends a list of course 
+ * codes which are required for the given major.
+ */
+const retrieveRequirements = async (req, res) => {
     console.group(); // Indents the following console logs
 
     const major = req.params.major;
@@ -16,10 +24,7 @@ const retrieveRequirements = async function (req, res) {
 
     // For each major the student is in, get the major requirements.
     for(let major of majors) {
-        let doc = db.collection('majors').doc(major);
-        let courseData = await doc.get();
-
-        console.log("courseData:", courseData);
+        let courseData = await db.collection('majors').doc(major).get();
 
         if (!courseData.exists) {
             console.error(`ERROR: ${major} does not exist in the DB.`);
@@ -45,34 +50,36 @@ const retrieveRequirements = async function (req, res) {
     console.groupEnd(); // Unindents
 }
 
-const build_semester = function (req, res) {
+
+/**
+ * Constructs a list of classes a student can take given 
+ * their previously taken courses, with a focus on major
+ * requirements.
+ * @param {object} req - request includes major with a 
+ * POST body of taken classes and major requirements.
+ * @param {object} res - repsonse includes an array of 
+ * courses described above.
+ */
+const build_semester = (req, res) => {
     let major = req.params.major;
     let { takenClasses, majorReqs } = req.body;
 
     // Run the flow
     let results = runFlow(major, takenClasses, majorReqs);
-    console.log('============================================')
-    console.log('results:', results);
 
     res.send(results);
 }
 
-/**
- * Obtains information about a particular course.
- */
-// const getCourseInfo = function (req, res) {
-//   let course_code = req.params.courseCode;
-//   var xmlHttp = new XMLHttpRequest.XMLHttpRequest();
-//   xmlHttp.open("GET", "https://one.ufl.edu/apix/soc/schedule/?category=CWSP&term=2211&course_code&course-code=" + course_code, false); // false for synchronous request
-//   xmlHttp.send(null);
-//   //   console.log(xmlHttp)
-//   res.send(xmlHttp.responseText);
-// }
 
-const getCourseInfo = async (req, res) => {
-    console.group();
+/**
+ * Uses the ONE.UF API to retrieve information about a particular course.
+ * @param {object} req - Pass the course code as a request parameter.
+ * @param {object} res - Sends response with course info.
+ */
+const getCourseInfo = (req, res) => {
     let course_code = req.params.courseCode;
-    console.log('Fetching course code info for', course_code);
+    
+    console.log('Fetching course code info for:', course_code);
 
     const data = __getCourseInfo(course_code);
 
@@ -80,48 +87,25 @@ const getCourseInfo = async (req, res) => {
 }
 
 
-
 /**
  * Tests the database by retrieving
  * a simple collection and displaying it.
  * NOTE: This is for debugging purposes only.
  */
-const testDB = async function (req, res) {
+const testDB = async (_, res) => {
 
     let major = 'ChemE';
-    const doc = db.collection('majors').doc(major);
-    const info = await doc.get();
+    const info = await db.collection('majors').doc(major).get();
 
     if (!info.exists) {
         console.log(`ERROR: ${major} does not exist in the DB.`);
-        res.send(404);
+        res.send(500);
     }
     else {
         res.send(info);
     }
 }
 
-/**
- * Gets a major document from the database.
- * TODO Unused at the moment
- */
-const getReqs = async function (req, res) {
-    const major = req.params.major;
-    const doc = db.collection('majors').doc(major);
-    const courseData = await doc.get();
-
-    if (!courseData.exists) {
-        console.log(`ERROR: ${major} does not exist in the DB.`);
-        res.send(`ERROR: ${major} does not exist in the DB.`);
-    }
-    else {
-        // Format as an array
-        let arr = courseData['_fieldsProto']['core']['arrayValue']['values'];
-        arr = arr.map(e => e['stringValue']);
-        console.log(arr);
-        res.send(arr);
-    }
-}
 
 export {
     retrieveRequirements,
