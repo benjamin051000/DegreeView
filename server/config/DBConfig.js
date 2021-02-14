664db7f7ec0e615ba37b12490446fd4995e68c77
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2021-02-14 14:40:25 -0500

    nodemon now no longer restarts from client changes

--- server/config/DBConfig.js
+++ server/config/DBConfig.js
@@ -3,7 +3,7 @@ import Firestore from '@google-cloud/firestore';
 /* Setup Google Cloud Firestore database. */
 const db = new Firestore({
     projectId: 'swamphacks-2021',
-    keyFilename: 'server/config/SwampHacks 2021-af160589d296.json',
+    keyFilename: './config/SwampHacks 2021-af160589d296.json',
 });
 
 export default db;
